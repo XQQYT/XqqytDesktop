565c460c109f1f4ec8c02488a699a58382754c3c
xqqyt <3338563384@qq.com>
2025-04-09 17:27:43 +0800

    add_eventbus

---
+++ Control/include/EventBus.h
@@ -0,0 +1,122 @@
+#ifndef _EVENTBUS_H
+#define _EVENTBUS_H
+
+#include <functional>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <iostream>
+#include <any>
+#include <memory>
+#include <type_traits>
+
+// function_traits 定义
+template<typename T>
+struct function_traits;
+
+// 普通函数
+template<typename Ret, typename... Args>
+struct function_traits<Ret(Args...)> {
+    using signature = Ret(Args...);
+};
+
+// 函数指针
+template<typename Ret, typename... Args>
+struct function_traits<Ret(*)(Args...)> : function_traits<Ret(Args...)> {};
+
+// std::function
+template<typename Ret, typename... Args>
+struct function_traits<std::function<Ret(Args...)>> : function_traits<Ret(Args...)> {};
+
+// 成员函数指针
+template<typename ClassType, typename Ret, typename... Args>
+struct function_traits<Ret(ClassType::*)(Args...)> : function_traits<Ret(Args...)> {};
+
+// const 成员函数指针
+template<typename ClassType, typename Ret, typename... Args>
+struct function_traits<Ret(ClassType::*)(Args...) const> : function_traits<Ret(Args...)> {};
+
+// 函数对象（包括lambda）
+template<typename Callable>
+struct function_traits : function_traits<decltype(&Callable::operator())> {};
+
+// 特化处理 std::bind
+template<typename Callable, typename... Args>
+struct function_traits<std::_Bind<Callable(Args...)>> 
+    : function_traits<Callable> {};
+
+using callback_id = size_t;
+
+class EventBus {
+public:
+    void registerEvent(const std::string& eventName) {
+        if (registered_events.find(eventName) == registered_events.end()) {
+            registered_events.emplace(eventName);
+            callbacks_map[eventName] = std::vector<CallbackWrapper>();
+            callbacks_map[eventName].reserve(4);
+            std::cout << "register " << eventName << std::endl;
+        }
+    }
+
+    // 显式模板参数的 subscribe
+    template<typename... Args>
+    callback_id subscribe(const std::string& eventName, std::function<void(Args...)> callback) {
+        if (!isEventRegistered(eventName)) {
+            throw std::runtime_error("Event not registered: " + eventName);
+        }
+        callback_id id = ++next_id;
+        callbacks_map[eventName].emplace_back(CallbackWrapper{id, callback});
+        return id;
+    }
+
+    // 自动推导 Callback 类型的 subscribe
+    template<typename Callback>
+    callback_id subscribe(const std::string& eventName, Callback&& callback) {
+        using signature = typename function_traits<std::decay_t<Callback>>::signature;
+        return subscribe(eventName, std::function<signature>(std::forward<Callback>(callback)));
+    }
+
+    template<typename... Args>
+    void publish(const std::string& eventName, Args... args) {
+        if (!isEventRegistered(eventName)) {
+            throw std::runtime_error("Event not registered: " + eventName);
+        }
+        for (auto& wrapper : callbacks_map[eventName]) {
+            try {
+                auto& func = std::any_cast<std::function<void(Args...)>&>(wrapper.callback);
+                func(args...);
+            } catch (const std::bad_any_cast&) {
+                throw std::runtime_error("Callback type mismatch for event: " + eventName);
+            }
+        }
+    }
+
+    bool isEventRegistered(const std::string& eventName) const {
+        return registered_events.find(eventName) != registered_events.end();
+    }
+
+    bool unsubscribe(const std::string& eventName, callback_id id) {
+        if (!isEventRegistered(eventName)) return false;
+        auto& callbacks = callbacks_map[eventName];
+        for (auto it = callbacks.begin(); it != callbacks.end(); ) {
+            if (it->id == id) {
+                it = callbacks.erase(it);
+                return true;
+            } else {
+                ++it;
+            }
+        }
+        return false;
+    }
+
+private:
+    struct CallbackWrapper {
+        callback_id id;
+        std::any callback;
+    };
+    std::unordered_map<std::string, std::vector<CallbackWrapper>> callbacks_map;
+    std::unordered_set<std::string> registered_events;
+    std::atomic<callback_id> next_id{0};
+};
+
+#endif

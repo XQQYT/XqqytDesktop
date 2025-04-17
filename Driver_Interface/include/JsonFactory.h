bf2d1084f2001ef00574969d78cad40c96368ca0
XQQYT <3338563384@qq.com>
2025-04-17 10:25:00 +0800

    fix_interface_from_class_to_abstract_class

--- Driver_Interface/include/JsonFactory.h
+++ Driver_Interface/include/JsonFactory.h
@@ -7,23 +7,23 @@
 class Parser
 {
 public:
-    virtual void loadJson(std::string&);
-    virtual std::string getKey(std::string&& key);
-    virtual std::unique_ptr<Parser> getObj(std::string&& key);
-    virtual bool contain(std::string&& ksssey);
-    virtual std::string toString();
+    virtual void loadJson(std::string&) = 0;
+    virtual std::string getKey(std::string&& key) = 0;
+    virtual std::unique_ptr<Parser> getObj(std::string&& key) = 0;
+    virtual bool contain(std::string&& ksssey) = 0;
+    virtual std::string toString() = 0;
 };
 class JsonFactory
 {
 public:
-    JsonFactory();
-    ~JsonFactory();
+    JsonFactory(){};
+    virtual ~JsonFactory(){};
     JsonFactory(const JsonFactory&) = delete;
-    virtual std::unique_ptr<Parser> getParser();
-    virtual std::shared_ptr<std::string> ws_register(const std::string&& id);
-    virtual std::shared_ptr<std::string> ws_message(const std::string&& user_id,const std::string&& target_id,const std::string&& msg);
-    virtual std::shared_ptr<std::string> ws_get_target_status(const std::string&& user_id,const std::string&& target_id);
-    virtual std::shared_ptr<std::string> ws_sdp_offer(const std::string&& user_id,const std::string&& target_id,const std::string&& sdp);
+    virtual std::unique_ptr<Parser> getParser() = 0;
+    virtual std::shared_ptr<std::string> ws_register(const std::string&& id) = 0;
+    virtual std::shared_ptr<std::string> ws_message(const std::string&& user_id,const std::string&& target_id,const std::string&& msg) = 0;
+    virtual std::shared_ptr<std::string> ws_get_target_status(const std::string&& user_id,const std::string&& target_id) = 0;
+    virtual std::shared_ptr<std::string> ws_sdp_offer(const std::string&& user_id,const std::string&& target_id,const std::string&& sdp) = 0;
 
 };
 

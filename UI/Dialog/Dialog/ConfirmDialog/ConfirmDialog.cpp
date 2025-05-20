56fd8fa7e4544d9d982b8f613f58b6d7b201adaf
XQQYT <3338563384@qq.com>
2025-05-20 15:04:02 +0800

    add_confirm_dialog

---
+++ UI/Dialog/Dialog/ConfirmDialog/ConfirmDialog.cpp
@@ -0,0 +1,40 @@
+/*
+ * XqqytDesktop
+ * Author: XQQYT
+ * License: MIT
+ * Year: 2025
+ */
+
+#include "ConfirmDialog.h"
+#include "ui_ConfirmDialog.h"
+
+ConfirmDialog::ConfirmDialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::ConfirmDialog)
+{
+    ui->setupUi(this);
+}
+
+ConfirmDialog::~ConfirmDialog()
+{
+    delete ui;
+}
+
+void ConfirmDialog::on_btn_yes_clicked()
+{
+    emit ChooseResult(true);
+    close();
+}
+void ConfirmDialog::on_btn_no_clicked()
+{
+    emit ChooseResult(false);
+    close();
+}
+
+void ConfirmDialog::exec(QString text, QString title)
+{
+    if(!title.isEmpty())
+        ui->label_title->setText(title);
+    ui->label_content->setText(text);
+    dynamic_cast<QDialog*>(this)->exec();
+}

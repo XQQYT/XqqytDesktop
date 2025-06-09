1bcaeb5da69bdbe4ccd421f50713b3732dad40ad
XQQYT <xqqyt0502@163.com>
2025-06-09 10:55:51 +0800

    update_build_sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Set path to your file
-file="/home/qyt/XqqytDesktop/CMakeLists.txt"
+file="./CMakeLists.txt"
 
 #Set the line
 lineNum=9
@@ -33,11 +33,36 @@ sudo apt-get install gcc g++ cmake -y
 
 sudo apt-get install libgl1-mesa-dev -y
 
+sudo apt-get install pkg-config -y
+
+sudo apt-get install libglib2.0-dev -y
+
 sudo apt-get install nlohmann-json3-dev -y
 
 sudo apt-get install libboost-all-dev -y
 
+sudo apt-get install libpulse-dev -y
+
+sudo apt-get install libssl-dev -y
+
+sudo apt-get install libx11-dev -y
+
+sudo apt-get install libxfixes-dev -y
+
+sudo apt-get install libxtst-dev -y
+
+sudo apt-get install libsodium-dev -y
+
+sudo apt-get install libxcomposite-dev libxrandr-dev libxrender-dev libxdamage-dev libgbm-dev libdrm-dev -y
+
 mkdir build
+
+cp -r Theme build/Theme
+cp -r Scripts build/Scripts
+cp -r Translations build/Translations
+cp -r User build/User
+cp devices-example.json build/devices.json
+cp settings-example.json build/settings.json
 
 cd build
 
